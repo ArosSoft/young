76a65126048cc01c2615551ab7d0139eb6a76a53
itkvant09user <itkvant09user>
2025-03-10 19:40:47 +0300

    я изменил хеловорлд надпись

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      1 программист написал приглашение на сайт и описал о чем он
+      Целая комманда разработчиков создала этот сайт и прокомментировала о чем он! <br>
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
       <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
     </h3>
